a73936403988a707c2fe224f10aea09f150ca4d6
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-03-22 11:46:01 +0100

    fix and

--- models_3.py
+++ models_3.py
@@ -285,7 +285,7 @@ class SegmentationModel_1(pl.LightningModule):
 		else:
 			x, times, y = batch
 		y_segm_H = torch.where(y>self.hparams.where_threshold_H, 1, 0).float()
-		y_segm_LH = torch.where((y<=self.hparams.where_threshold_H.bool() and y>=self.hparams.where_threshold_L.bool()), 1, 0).float()
+		y_segm_LH = torch.where((y<=self.hparams.where_threshold_H.bool() & y>=self.hparams.where_threshold_L.bool()), 1, 0).float()
 		y_segm_L = torch.where(y<self.hparams.where_threshold_L, 1, 0).float()
 		y_segm = torch.stack(y_segm_L, y_segm_LH, y_segm_H)
 		y_hat_segm = self.forward(x)
