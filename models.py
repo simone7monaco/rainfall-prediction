0f289b7c27beba5dd9301af6c70c0407ea5f673d
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-04-11 17:53:08 +0200

    add BS log

--- models.py
+++ models.py
@@ -246,6 +246,7 @@ class SegmentationModel(pl.LightningModule):
 			input_models_brier_score[lv] = ((prob_input_models - y_all.gt(lv).float())**2).mean().item()
 			print(f"Brier score for threshold {lv} mm: {brier_scores[lv]:.4f}")
 			print(f">Brier score for input models: {input_models_brier_score[lv]:.4f}\n")
+			wandb.log({f"Brier score {lv} mm": brier_scores[lv]})
 		
 		import matplotlib.pyplot as plt
 		import seaborn as sns
