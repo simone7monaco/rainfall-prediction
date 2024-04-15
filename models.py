687398c556d7ddead329d5d3e7770d92aeb74749
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-04-15 17:54:31 +0200

    Revert "1"
    This reverts commit f6107d38082bfcb71724c5f54ed00833d731b20c.

--- models.py
+++ models.py
@@ -86,8 +86,8 @@ class SegmentationModel(pl.LightningModule):
 	
 	def training_step(self, batch, batch_idx):
 		x, y, ev_date = batch['x'], batch['y'], batch.get('ev_date')
-		y_hat = self.forward(x, ev_date) # shape (n_repetitions*n_samples, C, H, W)
-		loss = self.training_loss(y_hat, y)
+		#y_hat = self.forward(x, ev_date) # shape (n_repetitions*n_samples, C, H, W)
+		#loss = self.training_loss(y_hat, y)
   
 		lv_thresholds=[50/self.case_study_max]#, 5/self.case_study_max, 10/self.case_study_max, 20/self.case_study_max, 50/self.case_study_max, 100/self.case_study_max, 150/self.case_study_max]
 		probabilities = {lv: [] for lv in lv_thresholds}
@@ -100,11 +100,11 @@ class SegmentationModel(pl.LightningModule):
 
 		brier_score = self.brierLoss(probabilities, y)
       
-		self.train_losses.append([self.current_epoch, loss.item()])
-		self.log("train_L1loss", loss, prog_bar=True) 
+		#self.train_losses.append([self.current_epoch, loss.item()])
+		#self.log("train_L1loss", loss, prog_bar=True) 
 		self.log("train_brier_score", brier_score)
 
-		return loss
+		return brier_score #loss
 
 	def validation_step(self, batch, batch_idx):
 		x, y, ev_date = batch['x'], batch['y'], batch.get('ev_date')
