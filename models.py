b32f72d4608372571f27b6692373b007d97c13fa
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-04-15 15:56:40 +0200

    3

--- models.py
+++ models.py
@@ -30,7 +30,7 @@ class SegmentationModel(pl.LightningModule):
 		self.loss = nn.MSELoss()
 		self.training_loss = nn.L1Loss()
 		self.brierLoss = BrierLoss()
-		self.sigmoid = nn.Sigmoid()
+		self.sigmoid = nn.functional.sigmoid()
 
 		self.rmse = lambda loss: (loss*(self.case_study_max**2)).sqrt().item()
 		self.metrics = []
