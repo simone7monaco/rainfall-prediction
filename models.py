f7dd723260047e207dfe29a36cf3d6dc62ac8c46
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-04-15 15:54:22 +0200

    2

--- models.py
+++ models.py
@@ -89,12 +89,12 @@ class SegmentationModel(pl.LightningModule):
 		y_hat = self.forward(x, ev_date) # shape (n_repetitions*n_samples, C, H, W)
 		loss = self.training_loss(y_hat, y)
   
-		lv_thresholds=[1/self.case_study_max]#, 5/self.case_study_max, 10/self.case_study_max, 20/self.case_study_max, 50/self.case_study_max, 100/self.case_study_max, 150/self.case_study_max]
+		lv_thresholds=[1/self.case_study_max, 5/self.case_study_max, 10/self.case_study_max, 20/self.case_study_max, 50/self.case_study_max, 100/self.case_study_max, 150/self.case_study_max]
 		probabilities = {lv: [] for lv in lv_thresholds}
 		for i in range(20):
-			predictions = self.cnn(x) *self.mask.cuda() *self.case_study_max
+			predictions = self.cnn(x) *self.mask.cuda()
 			for lv in lv_thresholds:
-				probabilities[lv].append((self.sigmoid(predictions))) # -lv
+				probabilities[lv].append((self.sigmoid(predictions-lv)))
 		for lv in lv_thresholds:
 			probabilities[lv] = torch.stack(probabilities[lv], dim=0).mean(dim=0)
 
