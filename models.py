644335f6a9dd615474261f9a003958b695eba505
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-04-16 10:57:21 +0200

    only loss

--- models.py
+++ models.py
@@ -89,20 +89,20 @@ class SegmentationModel(pl.LightningModule):
 		y_hat = self.forward(x, ev_date) # shape (n_repetitions*n_samples, C, H, W)
 		loss = self.training_loss(y_hat, y)
   
-		lv_thresholds=[1/self.case_study_max, 5/self.case_study_max, 10/self.case_study_max, 20/self.case_study_max, 50/self.case_study_max, 100/self.case_study_max, 150/self.case_study_max]
-		probabilities = {lv: [] for lv in lv_thresholds}
-		for i in range(7):
-			predictions = self.forward(x, ev_date)
-			for lv in lv_thresholds:
-				probabilities[lv].append(self.sigmoid(predictions - lv))
-		for lv in lv_thresholds:
-			probabilities[lv] = torch.stack(probabilities[lv], dim=0).mean(dim=0)
+		# lv_thresholds=[1/self.case_study_max, 5/self.case_study_max, 10/self.case_study_max, 20/self.case_study_max, 50/self.case_study_max, 100/self.case_study_max, 150/self.case_study_max]
+		# probabilities = {lv: [] for lv in lv_thresholds}
+		# for i in range(7):
+		# 	predictions = self.forward(x, ev_date)
+		# 	for lv in lv_thresholds:
+		# 		probabilities[lv].append(self.sigmoid(predictions - lv))
+		# for lv in lv_thresholds:
+		# 	probabilities[lv] = torch.stack(probabilities[lv], dim=0).mean(dim=0)
 
-		brier_score = self.brierLoss(probabilities, y)
+		# brier_score = self.brierLoss(probabilities, y)
       
 		self.train_losses.append([self.current_epoch, loss.item()])
 		self.log("train_L1loss", loss, prog_bar=True) 
-		self.log("train_brier_score", brier_score)
+		#self.log("train_brier_score", brier_score)
 
 		return loss #brier_score/1000 + 
 
