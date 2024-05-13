ac7e9ff1cf5d880988177fc394b0f1e87745ee73
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-05-13 18:24:12 +0200

    print

--- models.py
+++ models.py
@@ -291,9 +291,9 @@ class SegmentationModel(pl.LightningModule):
         for metric in self.metrics:
             for j, th in enumerate(self.thresholds):
                 print("shape y:")
-                print(y_p[:, j, self.mask == 1].shape)
+                print(y_p[:, j, self.mask == 1])
                 print("shape prob:")
-                print(y_hat_prob[:, j, self.mask == 1].shape)
+                print(y_hat_prob[:, j, self.mask == 1])
                 self.log(
                     f"val/{metric.__name__} {th*self.case_study_max:.0f}",
                     metric(y_p[:, j, self.mask == 1], y_hat_prob[:, j, self.mask == 1]),
