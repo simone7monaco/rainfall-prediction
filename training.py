e31eec6e07ffeb92f4535dd51de5b3b999afda4d
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-05-15 17:16:13 +0200

    fix

--- training.py
+++ training.py
@@ -26,13 +26,13 @@ def get_args(args=None):
     # parser.add_argument("--split_idx", type=str, default="701515")
     parser.add_argument("--n_split", type=int, default=8)
     parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--epochs", "-e", type=int, default=1)
+    parser.add_argument("--epochs", "-e", type=int, default=0)
     parser.add_argument("--load_checkpoint", type=Path, default=None)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--forward_passes", type=int, default=1)
     parser.add_argument("--code_version", type=int, default=5)
     parser.add_argument("--fine_tune", type=int, default=1)
-    parser.add_argument("--epochs_fn", "-f", type=int, default=50)
+    parser.add_argument("--epochs_fn", "-f", type=int, default=150)
     parser.add_argument("--finetune_type", type=str, default="bin", choices=["mine", "bin", "kde"])
     args = parser.parse_args(args)
     return args
@@ -57,52 +57,53 @@ def main(args):
         fine_tune = 1
         args.fine_tune = 0
 
-    if args.epochs > 1 and not args.load_checkpoint:
-        logger = WandbLogger(project="rainfall_prediction")
-        # add your batch size to the wandb config
-        logger.experiment.config["batch_size"] = 32
+    logger = WandbLogger(project="rainfall_prediction")
+    # add your batch size to the wandb config
+    logger.experiment.config["batch_size"] = 32
+
+    if  args.epochs>0:
+        if not args.load_checkpoint:
+            early_stop = EarlyStopping(monitor="val/loss", min_delta=0.00, patience=25, verbose=False, mode="min")
+            model_checkpoint = ModelCheckpoint(
+                output_path / f"split_{args.n_split}",
+                monitor="val/loss",
+                mode="min",
+                filename="{epoch}-{val/loss:.2f}",
+            )
+
+            model = SegmentationModel(**args.__dict__)
+            trainer = pl.Trainer(
+                accelerator="gpu" if cuda.is_available() else "cpu",
+                max_epochs=args.epochs,
+                callbacks=[model_checkpoint, early_stop],
+                log_every_n_steps=1,
+                logger=logger,  # default is TensorBoard
+            )
+            trainer.fit(model)
+
+            print(f"\nLoading best model ({model_checkpoint.best_model_path})")
+            model = SegmentationModel.load_from_checkpoint(
+                model_checkpoint.best_model_path,
+                fine_tune=fine_tune,
+                finetune_type=args.finetune_type,
+            )
+        else:
+            trainer = pl.Trainer(accelerator="gpu" if cuda.is_available() else "cpu")
+            print(f"\n⬆️  Loading checkpoint {args.load_checkpoint}")
+            model = SegmentationModel.load_from_checkpoint(
+                args.load_checkpoint, fine_tune=fine_tune, finetune_type=args.finetune_type
+            )
     else:
-        # logger = WandbLogger(project='rainfall_prediction_trash')
-        logger = WandbLogger(project="rainfall_prediction")
-
-    if not args.load_checkpoint:
-        early_stop = EarlyStopping(monitor="val/loss", min_delta=0.00, patience=25, verbose=False, mode="min")
-        model_checkpoint = ModelCheckpoint(
-            output_path / f"split_{args.n_split}",
-            monitor="val/loss",
-            mode="min",
-            filename="{epoch}-{val/loss:.2f}",
-        )
-
+        args.fine_tune = 1
         model = SegmentationModel(**args.__dict__)
-        trainer = pl.Trainer(
-            accelerator="gpu" if cuda.is_available() else "cpu",
-            max_epochs=args.epochs,
-            callbacks=[model_checkpoint, early_stop],
-            log_every_n_steps=1,
-            logger=logger,  # default is TensorBoard
-        )
-        trainer.fit(model)
-
-        print(f"\nLoading best model ({model_checkpoint.best_model_path})")
-        model = SegmentationModel.load_from_checkpoint(
-            model_checkpoint.best_model_path,
-            fine_tune=fine_tune,
-            finetune_type=args.finetune_type,
-        )
-    else:
-        trainer = pl.Trainer(accelerator="gpu" if cuda.is_available() else "cpu")
-        print(f"\n⬆️  Loading checkpoint {args.load_checkpoint}")
-        model = SegmentationModel.load_from_checkpoint(
-            args.load_checkpoint, fine_tune=fine_tune, finetune_type=args.finetune_type
-        )
+    
 
     if fine_tune == 1:
         model_checkpoint = ModelCheckpoint(
             output_path / f"split_{args.n_split}",
-            monitor="val/loss",
+            monitor="val/ECE",
             mode="min",
-            filename="{epoch}-{val/loss:.4f}",
+            filename="{epoch}-{val/ECE:.4f}",
         )
         trainer = pl.Trainer(
             accelerator="gpu" if cuda.is_available() else "cpu",
