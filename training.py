cd15dd9c5022632222c5d07736afde2c959ef06e
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-05-24 15:41:07 +0200

    oks

--- training.py
+++ training.py
@@ -34,10 +34,10 @@ def get_args(args=None):
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--forward_passes", type=int, default=1)
     parser.add_argument("--code_version", type=int, default=5)
-    parser.add_argument("--fine_tune", type=int, default=0)
+    parser.add_argument("--fine_tune", type=int, default=1)
     parser.add_argument("--n_thresh", type=int, default=7)
     parser.add_argument("--indx_thresh", type=int, default=0)
-    parser.add_argument("--epochs_fn", "-f", type=int, default=50)
+    parser.add_argument("--epochs_fn", "-f", type=int, default=80)
     parser.add_argument("--finetune_type", type=str, default="bin", choices=["mine", "bin", "kde"])
     args = parser.parse_args(args)
     return args
