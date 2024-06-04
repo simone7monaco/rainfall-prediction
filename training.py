d000b7750069e18bec461e143a460e3435f607e9
SimoneFassio <123181098+SimoneFassio@users.noreply.github.com>
2024-06-04 17:09:59 +0200

    fine

--- training.py
+++ training.py
@@ -33,7 +33,7 @@ def get_args(args=None):
     parser.add_argument("--load_checkpoint", type=Path, default=None)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--code_version", type=int, default=5)
-    parser.add_argument("--fine_tune", type=int, default=0)
+    parser.add_argument("--fine_tune", type=int, default=1)
     parser.add_argument("--n_thresh", type=int, default=1)
     parser.add_argument("--indx_thresh", type=int, default=1)
     parser.add_argument("--epochs_fn", "-f", type=int, default=15)
